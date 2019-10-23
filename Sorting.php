33a97c934f1ebb8d679d30dcfb7f417c3a445127
prifulnath <prifulprem@gmail.com>
2019-10-24 00:03:14 +0530

    Bubble Sorting

--- Sorting.php
+++ Sorting.php
@@ -13,10 +13,18 @@ class Sort
 	 */
 	public function bubbleSort($arrValues)
 	{
-		print_r($arrValues);
-		print('test');
+		$numCount = count($arrValues);
+		for ($i=$numCount-1; $i > 0; $i--) {
+			for ($j=0; $j < $i; $j++) {
+				if ($arrValues[$j] > $arrValues[$j+1]) {
+					$numTemp         = $arrValues[$j];
+					$arrValues[$j]   = $arrValues[$j+1];
+					$arrValues[$j+1] = $numTemp;
+				}
+			}
+		}
 	}
 }
 
 $objSort = new Sort();
-$objSort->bubbleSort([5,2,4,7,6,8]);
+$objSort->bubbleSort([5,2,8,7,6,4,1]);
