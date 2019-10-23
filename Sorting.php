8bbef064911562b2a5458d7b21942b023441644c
prifulnath <prifulprem@gmail.com>
2019-10-24 00:23:21 +0530

    Selection Sorting

--- Sorting.php
+++ Sorting.php
@@ -9,10 +9,31 @@ class Sort
 	function __construct(){}
 
 	/**
-	 * Bubble sorting function
+	 * The selection sort algorithm sorts an array 
+	 * by finding the minimum element from unsorted part 
+	 * and putting it at the beginning
 	 */
-	public function bubbleSort($arrValues)
-	{
+	public function selectionSort($arrValues) {
+		for ($i=0; $i < count($arrValues); $i++) {
+			$minVal = $arrValues[$i];
+			$minInd = $i;
+			for ($j=$i+1; $j < count($arrValues); $j++) { 
+				if ($arrValues[$j] < $minVal) {
+					$minVal = $arrValues[$j];
+					$minInd = $j;
+				}
+			}
+			$arrValues[$minInd] = $arrValues[$i];
+			$arrValues[$i] = $minVal;
+		}
+		return $arrValues;
+	}
+
+	/**
+	 * Bubble Sort works by swapping the adjacent elements 
+	 * if they are in wrong order.
+	 */
+	public function bubbleSort($arrValues) {
 		$numCount = count($arrValues);
 		for ($i=$numCount-1; $i > 0; $i--) {
 			for ($j=0; $j < $i; $j++) {
@@ -23,8 +44,10 @@ class Sort
 				}
 			}
 		}
+		return $arrValues;
 	}
 }
 
 $objSort = new Sort();
-$objSort->bubbleSort([5,2,8,7,6,4,1]);
+$arrSorted = $objSort->bubbleSort([5,2,8,7,6,4,1]);    echo '<br>'; print_r($arrSorted);
+$arrSorted = $objSort->selectionSort([5,2,8,7,6,4,1]); echo '<br>'; print_r($arrSorted);
